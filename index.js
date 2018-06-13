e720acf29e166547a172fd44e4eee0cedf8510f6
Thomas Lever <thomaslever508@gmail.com>
2018-06-13 14:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ function keepGoing(array, changeValue, skipValue){
   }
 }
 
-function findBy(array, findFn)
+function findBy(array, findFn){
+  for (let i = 0; i < array.length; i++){
+    if (findFN(array[i])){
+      return array[i]
+    } 
+  }
+}
