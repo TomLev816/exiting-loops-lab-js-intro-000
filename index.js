67dcc834a1d01d3ddbf92771ec0ca302671c472f
Thomas Lever <thomaslever508@gmail.com>
2018-06-13 14:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ function breakOut(array, changeValue, stopValue){
 }
 
 function keepGoing(array, changeValue, skipValue){
-
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === skipValue){
+      continue
+    } else {
+      array[i] = changeValue
+    }
+  }
 }
 
 function findBy(array, findFn)
